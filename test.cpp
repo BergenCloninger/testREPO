8dc95d136c51956196ce575406aace9914b763d2
BergenCloninger <105687249+BergenCloninger@users.noreply.github.com>
2023-01-28 18:26:47 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     for (int i = 0; i < 100; i++) {
-        std::cout << "Hewwo uwu";
+        std::cout << "Hewwo uwu *eat's your ass :)*";
     }
     for(int i = 0; i < 100; i++){
         std::cout << "Skrunkly !!!! :)))))))))))))";
