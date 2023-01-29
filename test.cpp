7f05635e899c6d94b03e8cd429e04293cc96dec6
Intaiachi <mjones21031@gmail.com>
2023-01-28 18:27:00 -0700

    poinglrbop

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     for (int i = 0; i < 100; i++) {
-        std::cout << "Hewwo uwu";
+        std::cout << "hemorshporgbang :)____))))))))))))))))))";
     }
     for(int i = 0; i < 100; i++){
         std::cout << "Skrunkly !!!! :)))))))))))))";
