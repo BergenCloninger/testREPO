a603b1715d51496fadcd18b129054c42a7a7ea17
Intaiachi <mjones21031@gmail.com>
2023-01-28 18:19:23 -0700

    whoah

--- test.cpp
+++ test.cpp
@@ -4,4 +4,7 @@ int main() {
     for (int i = 0; i < 100; i++) {
         std::cout << "Hewwo uwu *nuzzles you*";
     }
+    for(int i = 0; i < 100; i++){
+        std::cout << "Skrunkly !!!! :)))))))))))))";
+    }
 }
